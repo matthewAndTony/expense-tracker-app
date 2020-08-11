80a5a63724435ae3acfc39e0645600038703b961
tony salloum <antoine.salloum@gmail.com>
2020-08-11 12:56:36 -0400

    added percentages for expenses

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -8,6 +8,28 @@ class Results extends Component {
             <div>
                 <h2>Weekly Savings</h2>
                 <p>${this.props.userResults.totalIncome - this.props.userResults.totalExpense - this.props.userResults.totalMiscExpense}</p>
+
+                <h2>Percentage of Expenses</h2>
+                {Object.keys(this.props.userResults.addedExpense).map((expense)=>{
+                    return(   
+                        // percentage of expenses for each expense 
+                        // expense / total expense
+                        // need to create p tag
+                        <p>${parseInt(expense, 10)/ this.props.userResults.totalExpense}</p>
+
+                    )
+                })}
+
+                <h2>Percentage of One Off Expenses</h2>
+                {Object.keys(this.props.userResults.addedMiscExpense).map((miscExpense)=>{
+                    return(   
+                        // percentage of expenses for each expense 
+                        // expense / total expense
+                        // need to create p tag
+                        <p>${parseInt(miscExpense, 10) / this.props.userResults.totalMiscExpense}</p>
+
+                    )
+                })}
             </div>
         )}
 }
